e9419db8ce6f13979bef068bc983528a676546d5
ColeSny <76003944+ColeSny@users.noreply.github.com>
2024-11-26 01:11:45 -0500

    v2 Debugging, forgot break statement for switch. While loop error. Cleaned up UI.

--- ToDoListManager.java
+++ ToDoListManager.java
@@ -9,34 +9,42 @@ public class ToDoListManager {
         ArrayList<String> tasks = new ArrayList<>();
         ArrayList<Boolean> status = new ArrayList<>();
 
-        @SuppressWarnings("unused") //its used when option 4 is selected.
         Boolean exit = false;
-        
-        while(exit = false){
+
+        System.out.println("Welcome to the To Do list manager.");
+
+        while(exit != true){
+
             int choice = menu();
+            kb.nextLine();
             switch(choice){
                 case 1:
                     System.out.print("\nEnter the task: ");
                     String task = kb.nextLine();
                     tasks.add(task);
                     status.add(false);
+
+                    break;
                 case 2:
-                    System.out.print("\nYour tasks: ");
+                    System.out.print("\nYour tasks: \n");
                     for(int i =0; i <tasks.size(); i++){
-                        System.out.println(tasks.get(i) +"Status: " + status.get(i));
+                        System.out.println(tasks.get(i) +" | Status: " + status.get(i) + "\n");
                     }
+                    break;
                 case 3:
                     System.out.print("\nEnter the task number to mark as completed: ");
                     int pos = kb.nextInt();
                     if(pos > 0  && pos <= tasks.size()){
                         status.set(pos-1, true);
-                        System.out.println("Task complete!");
+                        System.out.println("Task complete! \n");
                     }else{
-                        System.out.println("Invalid number.");
+                        System.out.println("Invalid number. \n");
                     }
+                    break;
                 case 4:
                     System.out.println("exiting...");
                     exit = true;
+                    break;
             }
         }
         System.out.println("Thank you! \nGoodbye!");
@@ -44,9 +52,7 @@ public class ToDoListManager {
 
     private static int menu(){
         int choice = 0;
-        System.out.println("Welcome to the To Do list manager.");
         System.out.println("Please chose one of the following:");
-
         System.out.println("1. Add a task");
         System.out.println("2. View Task");
         System.out.println("3. Mark task as complete");
